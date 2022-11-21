1b5f47949dd2b4136733c6f888ce653ed9b25910
shabana1579 <yasmeen.shabana1579@gmail.com>
2022-11-21 16:21:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+//function to clear the input box
+function clear()
+
+{
+   document.getElementsByTagName('input')[0].value="";
+   document.getElementsByTagName('input')[0].placeholder='0';
+    
+}
+
+//function to display the input
+function display(num){
+
+    document.getElementsByTagName('input')[0].value+=num;
+
+}
+
+//function to calculate using eval() function
+function equal()
+{
+    var exp=document.getElementsByTagName('input')[0].value;
+    
+   if(exp)
+    {
+     document.getElementsByTagName('input')[0].value= eval(exp)
+    }
+    else 
+    {alert("Invalid Input")}
+    
+    if(document.getElementsByTagName('input')[0].value==0)
+    {
+        
+        document.getElementsByTagName('input')[0].value=''
+
+    }
+
+}
+
+
+
+//function to delete the input from right to left one by one
+function delete1()
+{
+    var exp=document.getElementsByTagName('input')[0].value;
+
+    document.getElementsByTagName('input')[0].value = exp.substring(0,exp.length-1);
+
+    if(document.getElementsByTagName('input')[0].value==0)
+    {
+        
+        document.getElementsByTagName('input')[0].value=''
+
+    }
+}
